b316aaa020c797964c4b0fb96be7e12179762db1
Eragon35 <antipin35@yahoo.com>
2021-02-19 23:46:36 +0300

    finish problem A

--- A.cpp
+++ A.cpp
@@ -3,26 +3,23 @@ using namespace std;
 
 int main() {
     int n = 10;
-    int max = 0, left = 0, right = 0, currnetMax = 0, currentLeft = 1;
+    int max = 0, left = 0, right = 0, same = 1, fleft = 0;
     cin >> n;
     int array[200000];
     for (int i = 0; i < n; i++)  cin >> array[i];
-    for (int i = 2; i < n; i++)  {
-        if ((array[i] != array[i - 1]) && (array[i - 1] != array[i - 2])) currnetMax++;
-        else {
-            if (currnetMax > max){
-                max = currnetMax;
-                left = currentLeft+1;
-                right = i;
-            }
-            currentLeft = i;
-            currnetMax = 0;
+    for (int i = 1; i < n; i++)  {
+        if (array[i] == array[i-1]) same++;
+        else same = 1;
+        if (same >= 3) {
+            // фотография закончилась
+            left = i - 1;
+        }
+        else if (i - left > max){
+            right = i;
+            max = i - left;
+            fleft = left;
         }
     }
-    if (currnetMax > max){
-        left = currentLeft-1;
-        right = n;
-    }
-    cout << left << " " << right;
+    cout << fleft+1 << " " << right+1;
     return 0;
 }
