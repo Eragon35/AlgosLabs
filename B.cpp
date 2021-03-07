fe77dd3019ff3fca87062939490200ee639b3470
Eragon35 <antipin35@yahoo.com>
2021-03-07 11:54:35 +0300

    Fix answer for 20+ input

--- B.cpp
+++ B.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 struct Crap{
@@ -8,8 +9,13 @@ struct Crap{
     int counter = 0;
     int position {};
 };
+struct Answer{
+    int position {};
+    int counter {};
+};
 int inputSize;
 string input;
+vector<Answer> answer;
 
 void printCrap(vector<Crap> vtc){
     for (int i = 0; i < vtc.size(); i++){
@@ -17,25 +23,53 @@ void printCrap(vector<Crap> vtc){
     }
 }
 
+bool comp(Answer ans, Answer awr){
+    return ans.position < awr.position;
+}
+
 vector<Crap> reduce(vector<Crap> line) {
-    int i = 1;
+    int i = 0;
     int size = line.size();
+
     while (i < size){
-        if (abs(line[i].symbol - line[i-1].symbol) == 32) {
-            Crap upper, lower;
-            if (line[i].symbol > line[i-1].symbol) {
-                upper = line[i-1];
-                lower = line[i];
-            } else {
-                upper = line[i];
-                lower = line[i-1];
-            }
-            input[upper.position] = lower.counter + 48;
-//            cout << lower.counter << endl;
-            line.erase(line.begin() + i- 1, line.begin() + i + 1);
-            size = line.size();
+        if (i == 0){
+            if (abs(line[i].symbol - line[line.size()-1].symbol) == 32) {
+                Crap upper, lower;
+                if (line[i].symbol > line[line.size()-1].symbol) {
+                    upper = line[line.size()-1];
+                    lower = line[i];
+                } else {
+                    upper = line[i];
+                    lower = line[line.size()-1];
+                }
+                Answer awr;
+                awr.position = upper.position;
+                awr.counter = lower.counter;
+                answer.push_back(awr);
+//                answer[upper.position].counter -= lower.counter;
+                line.erase(line.begin());
+                line.pop_back();
+                size = line.size();
+            } else i++;
+        } else {
+            if (abs(line[i].symbol - line[i - 1].symbol) == 32) {
+                Crap upper, lower;
+                if (line[i].symbol > line[i - 1].symbol) {
+                    upper = line[i - 1];
+                    lower = line[i];
+                } else {
+                    upper = line[i];
+                    lower = line[i - 1];
+                }
+                Answer awr;
+                awr.position = upper.position;
+                awr.counter = lower.counter;
+                answer.push_back(awr);
+//                answer[upper.position].counter -= lower.counter;
+                line.erase(line.begin() + i - 1, line.begin() + i + 1);
+                size = line.size();
+            } else i++;
         }
-        else i++;
     }
     return line;
 }
@@ -58,22 +92,16 @@ int main() {
         sizeBefore = temp.size();
         temp = reduce(temp);
         sizeAfter = temp.size();
-//        printCrap(temp);
-//        cout << endl;
-//        cout << sizeBefore << " " << sizeAfter << endl;
     }
-
-    if (temp.size() != 0) {
-//        cout << input << endl;
-        cout << "Impossible" << endl;
+    if (temp.empty()) {
+        cout << "Possible" << endl;
+        sort(answer.begin(), answer.end(), comp);
+        for (Answer c : answer){
+            cout << c.counter << " ";
+            }
         return 0;
     } else{
-        cout << "Possible" << endl;
-//        cout << input.size() << endl;
-//        cout << input;
-        for (char i : input){
-            if (i < 60) cout << i << " ";
-        }
+        cout << "Impossible";
+        return 0;
     }
-    return 0;
 }
