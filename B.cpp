15914a1897e22269b9375971316e5b6f679cffe1
Eragon35 <antipin35@yahoo.com>
2021-03-10 23:48:22 +0300

    Rework to list instead of vector, but it crash sometimes

--- B.cpp
+++ B.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <list>
 #include <algorithm>
 using namespace std;
 
@@ -10,45 +11,76 @@ struct Crap{
 };
 int inputSize;
 string input;
-vector<Crap> temp;
+//vector<Crap> temp;
+list<Crap> temp = {};
 vector<int> answer;
+
+void printCrap(list<Crap> vtc){
+    for (auto & iter : vtc){
+        cout << iter.symbol << " " << iter.number << endl;
+    }
+    cout << "---------------------------" << endl;
+}
 
 void reduce() {
     int i = 0;
     int size = temp.size();
+    auto it = temp.begin();
 
     while (i < size){
-        if (i == 0){
-            if (abs(temp[i].symbol - temp[temp.size() - 1].symbol) == 32) {
+        cout << "pidor";
+
+        if (it == temp.begin()){
+            if (abs(temp.front().symbol - temp.back().symbol) == 32) {
                 Crap upper, lower;
-                if (temp[i].symbol > temp[temp.size() - 1].symbol) {
-                    upper = temp[temp.size() - 1];
-                    lower = temp[i];
+                if (temp.front().symbol > temp.back().symbol) {
+                    upper = temp.back();
+                    lower = temp.front();
                 } else {
-                    upper = temp[i];
-                    lower = temp[temp.size() - 1];
+                    upper = temp.front();
+                    lower = temp.back();
                 }
                 answer[upper.number] = lower.number;
-                temp.erase(temp.begin());
-                temp.pop_back();
+//                temp.erase(temp.begin());
+//                temp.pop_back();
+                    temp.pop_front();
+                    temp.pop_back();
                 size = temp.size();
-            } else i++;
+                it = temp.begin();
+                cout << "deleted first & last" << endl;
+                printCrap(temp);
+            }
+            advance(it, 1);
+            i++;
         } else {
-            if (abs(temp[i].symbol - temp[i - 1].symbol) == 32) {
+            auto pv = prev(it, 1);
+            auto nt = it;
+            if (abs((*it).symbol - (*pv).symbol) == 32) {
                 Crap upper, lower;
-                if (temp[i].symbol > temp[i - 1].symbol) {
-                    upper = temp[i - 1];
-                    lower = temp[i];
+                if ((*it).symbol > (*pv).symbol) {
+                    upper = *pv;
+                    lower = *it;
                 } else {
-                    upper = temp[i];
-                    lower = temp[i - 1];
+                    upper = *it;
+                    lower = *pv;
                 }
                 answer[upper.number] = lower.number;
-                temp.erase(temp.begin() + i - 1, temp.begin() + i + 1);
+//                temp.erase(temp.begin() + i - 1, temp.begin() + i + 1);
+//                temp.remove(upper);
+//                temp.remove(lower);
+                temp.erase(pv);
+                temp.erase(it);
+                cout << "delete inside" << endl;
+                printCrap(temp);
                 size = temp.size();
-            } else i++;
+                it = temp.begin();
+                advance(it, i);
+            }
+            it = temp.begin();
+            advance(it, i++);
         }
     }
+    cout << "end of reduce" << endl;
 }
 
 int main() {
@@ -63,13 +95,17 @@ int main() {
         else crap.number = position++;
         temp.push_back(crap);
     }
+    printCrap(temp);
     int sizeBefore = 0;
     while (sizeBefore != temp.size()){
         sizeBefore = temp.size();
         reduce();
+//        printCrap(temp);
     }
     if (!temp.empty()) {
         cout << "Impossible";
+//        cout << endl;
+//        printCrap(temp);
     } else{
         cout << "Possible" << endl;
         for (int c : answer){
